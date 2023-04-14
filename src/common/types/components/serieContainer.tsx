f620d9b735f0353ddadd5e0a973004b9313fc0b3
Pablo Di Martino <pablo.dimartino@disney.com>
2023-04-14 13:42:50 -0300

    Update serieContainer.tsx

--- src/common/types/components/serieContainer.tsx
+++ src/common/types/components/serieContainer.tsx
@@ -9,7 +9,7 @@ export default function SerieContainer(props: Props) {
     return (
         <div className='flex flex-row overflow-x-auto pt-4 bg-gray-300'>
             {props.series.map((serie, index) => (
-                <div className="flex flex-shrink-0 flex-col w-32 h-[19rem] sm:w-52 sm:h-[22rem] ml-2 mr-2">
+                <div key={`serie${serie.id}`} className="flex flex-shrink-0 flex-col w-32 h-[19rem] sm:w-52 sm:h-[22rem] ml-2 mr-2">
                     <div className="flex w-full pt-1 h-72 justify-center">
                         <Image className="rounded-3xl" src={`${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_BACKDROP_SIZE}${serie.poster_path}`} width={200} height={200} style={{objectFit: 'contain',}} alt=""/>
                     </div>
