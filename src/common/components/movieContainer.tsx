fae3069e23bc710b6277cbc3052b328fbaa794c9
Pablo Di Martino <pablo.dimartino@disney.com>
2023-04-16 17:02:53 -0300

    blur image

--- src/common/components/movieContainer.tsx
+++ src/common/components/movieContainer.tsx
@@ -13,12 +13,18 @@ export default function MovieContainer(props: Props) {
 
 
     return (
-        <div className='flex flex-row overflow-x-auto pt-4 bg-gray-300'>
+        <div className='flex flex-row overflow-x-auto overflow-y-hidden pt-4 bg-gray-300'>
             {props.movies.map((movie, index) => (
                 <div key={`movie${movie.id}`} className="flex flex-shrink-0 flex-col w-32 h-72 sm:w-52 sm:h-96  ml-2 mr-2">
                     <Link href={`/${props.country}/movie/${movie.id}?language=${props.language}`}>
                         <div className="flex w-full pt-1 h-48 sm:h-80 justify-center relative">
-                            <Image className="rounded-3xl" src={`${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD}${movie.poster_path}`} fill style={{objectFit: 'cover',}} sizes={imageSizes} alt={movie.title}/>
+                            <Image className="rounded-3xl" 
+                                    src={`${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD}${movie.poster_path}`} 
+                                    fill
+                                    placeholder='blur'
+                                    blurDataURL={`/_next/image?url=${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD}${movie.poster_path}&w=16&q=1`} 
+                                    style={{objectFit: 'cover',}} 
+                                    sizes={imageSizes} alt={movie.title}/>
                         </div>
                         <div className="flex w-full pl-2 text-left">
                             <h3>{movie.title}</h3>
--- src/common/components/serieContainer.tsx
+++ src/common/components/serieContainer.tsx
@@ -10,11 +10,17 @@ export default function SerieContainer(props: Props) {
 
 
     return (
-        <div className='flex flex-row overflow-x-auto pt-4 bg-gray-300'>
+        <div className='flex flex-row overflow-x-auto overflow-y-hidden pt-4 bg-gray-300'>
             {props.series.map((serie, index) => (
                 <div key={`movie${serie.id}`} className="flex flex-shrink-0 flex-col w-32 h-72 sm:w-52 sm:h-96  ml-2 mr-2">
                     <div className="flex w-full pt-1 h-48 sm:h-80 justify-center relative">
-                        <Image className="rounded-3xl" src={`${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD}${serie.poster_path}`} fill style={{objectFit: 'cover',}} sizes={imageSizes} alt={serie.name}/>
+                    <Image className="rounded-3xl" 
+                                    src={`${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD}${serie.poster_path}`} 
+                                    fill
+                                    placeholder='blur'
+                                    blurDataURL={`/_next/image?url=${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD}${serie.poster_path}&w=16&q=1`} 
+                                    style={{objectFit: 'cover',}} 
+                                    sizes={imageSizes} alt={serie.name}/>
                     </div>
                      <div className="flex w-full pl-2 text-left">
                         <h3>{serie.name}</h3>
