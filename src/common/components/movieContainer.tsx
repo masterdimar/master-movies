b9da3860aa3ddf35582470809ee2642ec7dfec90
lvcinfluenza <lvcinfluenza@gmail.com>
2023-05-02 12:41:35 -0300

    Flechas...Los DG son todos Putos!

--- src/common/components/movieContainer.tsx
+++ src/common/components/movieContainer.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { TMDBMovie } from "../types/tmdbMovie";
 import Link from 'next/link';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 type Props ={
   movies: TMDBMovie[],
@@ -10,9 +11,20 @@ type Props ={
 export default function MovieContainer(props: Props) {
     const imageSizes: string = '(max-width: 250px) 100vw, (max-width: 500px) 50vw, (max-width: 999px) 20vw, 10vw'
 
+    const slideLeft = () => {
+        var slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft - 500
+    }
+
+    const slideRight = () => {
+        var slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft + 500
+    }
 
     return (
-        <div className='contenedorThumbs'>
+        <div className='relative flex items-center'>
+            <MdChevronLeft onClick={slideLeft} className='flechas'/>
+            <div id="slider" className='contenedorThumbs'>
             {props.movies.map((movie, index) => (
                 <div key={`movie${movie.id}`} className="thumbBox">
                     <Link href={`/movie/${movie.id}?language=${props.language}`}>
@@ -32,6 +44,8 @@ export default function MovieContainer(props: Props) {
                 </div>
                 
 			))}
+        </div>
+        <MdChevronRight onClick={slideRight} className='flechas'/>
         </div>
     )
 }
--- src/common/components/serieContainer.tsx
+++ src/common/components/serieContainer.tsx
@@ -11,30 +11,40 @@ type Props ={
 export default function SerieContainer(props: Props) {
     const imageSizes: string = '(max-width: 250px) 100vw, (max-width: 500px) 50vw, (max-width: 999px) 20vw, 10vw'
 
+    const slideLeft = () => {
+        var slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft - 500
+    }
 
+    const slideRight = () => {
+        var slider = document.getElementById('slider')
+        slider.scrollLeft = slider.scrollLeft + 500
+    }
     return (
-        <div className='contenedorThumbs'>
-            <MdChevronLeft size={40} color='white'/>
-            {props.series.map((serie, index) => (
-                <div key={`serie${serie.id}`} id='slider' className="thumbBox">
-                    <Link href={`/serie/${serie.id}?language=${props.language}`}>
-                        <div className="thumbIMG">
-                            <Image className="thumbIMGBorder" 
-                                        src={`${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD}${serie.poster_path}`} 
-                                        fill
-                                        placeholder='blur'
-                                        blurDataURL={`/_next/image?url=${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD_MIN}${serie.poster_path}&w=16&q=1`} 
-                                        style={{objectFit: 'cover',}} 
-                                        sizes={imageSizes} alt={serie.name}/>
-                        </div>
-                        <div className="thumbTXT">
-                            <h3>{serie.name}</h3>
-                        </div>
-                    </Link> 
-                </div>
-                
-			))}
-        <MdChevronRight size={40} />
+        <div className='relative flex items-center'>
+            <MdChevronLeft onClick={slideLeft} className='flechas'/>
+            <div id="slider" className='contenedorThumbs'>
+                {props.series.map((serie, index) => (
+                    <div key={`serie${serie.id}`} className="thumbBox">
+                        <Link href={`/serie/${serie.id}?language=${props.language}`}>
+                            <div className="thumbIMG">
+                                <Image className="thumbIMGBorder" 
+                                            src={`${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD}${serie.poster_path}`} 
+                                            fill
+                                            placeholder='blur'
+                                            blurDataURL={`/_next/image?url=${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD_MIN}${serie.poster_path}&w=16&q=1`} 
+                                            style={{objectFit: 'cover',}} 
+                                            sizes={imageSizes} alt={serie.name}/>
+                            </div>
+                            <div className="thumbTXT">
+                                <h3>{serie.name}</h3>
+                            </div>
+                        </Link> 
+                    </div>
+                    
+                ))}
+            </div>
+        <MdChevronRight onClick={slideRight} className='flechas'/>
         </div>
     )
 }
