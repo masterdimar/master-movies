a8ac6f404b6f249a67576067a5b7c0442c5f830a
Pablo Di Martino <pablo.dimartino@disney.com>
2023-05-20 16:06:02 -0300

    Logos

--- src/common/components/watchProviderContainer.tsx
+++ src/common/components/watchProviderContainer.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { WatchProviderCountry } from "../types/watchProvider"
 
 type Props ={
@@ -13,6 +14,12 @@ export default function WatchProviderContainer(props: Props) {
                         {props.watchProvider.flatrate?.map((flatrate) => (                                   
                             <div key={`flatrate${flatrate.provider_id}`}  className="flex w-full pl-2 text-left">
                                 <h3>Mirala en {flatrate.provider_name} {flatrate.urlService}</h3>
+                                <Image className="thumbIMGBorder" 
+                                                src={`${process.env.THEMOVIEDB_BASE_URL}original/${flatrate.logo_path}`} 
+                                                width={80}
+                                                height={80}
+                                                style={{objectFit: 'cover',}} 
+                                                alt={flatrate.provider_name}/>
                             </div>                                                                            
                         ))}
                     </div>
@@ -25,6 +32,12 @@ export default function WatchProviderContainer(props: Props) {
                         {props.watchProvider.rent?.map((rent) => (                                   
                             <div key={`rent${rent.provider_id}`} className="flex w-full pl-2 text-left">
                                 <h3>Alquilala en {rent.provider_name} {rent.urlService}</h3>
+                                <Image className="thumbIMGBorder" 
+                                                src={`${process.env.THEMOVIEDB_BASE_URL}original/${rent.logo_path}`} 
+                                                width={80}
+                                                height={80}
+                                                style={{objectFit: 'cover',}} 
+                                                alt={rent.provider_name}/>
                             </div>                        
                         ))}
                     </div>
@@ -37,6 +50,12 @@ export default function WatchProviderContainer(props: Props) {
                         {props.watchProvider.buy?.map((buy) => (                                   
                             <div key={`buy${buy.provider_id}`} className="flex w-full pl-2 text-left">
                                 <h3>Comprala en {buy.provider_name} {buy.urlService}</h3>
+                                <Image className="thumbIMGBorder" 
+                                                src={`${process.env.THEMOVIEDB_BASE_URL}original/${buy.logo_path}`} 
+                                                width={80}
+                                                height={80}
+                                                style={{objectFit: 'cover',}} 
+                                                alt={buy.provider_name}/>
                             </div>                                                                        
                        ))}
                     </div>
