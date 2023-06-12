a9e848b1de8bc25583d296d4819e5d00ec97365e
lvcinfluenza <lvcinfluenza@gmail.com>
2023-06-12 14:22:35 -0300

    Update [id].tsx

--- src/pages/serie/[id].tsx
+++ src/pages/serie/[id].tsx
@@ -1,4 +1,3 @@
-
 import Image from 'next/image';
 import { GetServerSideProps, GetServerSidePropsContext } from "next"
 import { TMDBSerie } from "@/common/types/tmdbSerie"
@@ -16,6 +15,8 @@ type Props ={
   language: string
 }
  export default function Serie(props: Props) {
+  const imageSizes: string = '(max-width: 250px) 100vw, (max-width: 500px) 50vw, (max-width: 999px) 20vw, 10vw'
+
   let genres : string = ""
   props.serie.genres.map((genre: Genre) => {
     if(genres == "")
@@ -37,50 +38,65 @@ type Props ={
   return (
     <>       
       <main>
-          <div className="flex relative h-60 sm:h-96 md:h-[30rem] lg:h-[35rem] xl:h-[720px] 2xl:h-[864px] 3xl:h-[1080px] items-center bg-blue-900">
+
+          <div className="mainIMG">
             <Image src={`${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_BACKDROP_SIZE}${props.serie.backdrop_path}`} 
-              fill
-              priority
-              placeholder='blur'
-              blurDataURL={`/_next/image?url=${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_BACKDROP_SIZE}${props.serie.backdrop_path}&w=16&q=1`} 
-              style={{objectFit: 'cover', objectPosition: 'center top'}} 
-              alt={props.serie.name}/>
-            <div className="backdrop-brightness-50 bg-blue-600/5 w-full h-full p-3 sm:p-10 md:p-20 flex items-center">
-              <section className="text-left text-white w-full">
-                <div className="">
-                  <h3 className="text-md sm:text-lg uppercase">Serie</h3>
-                </div>
-                <div className="">
-                  <h1 className="font-extrabold text-2xl sm:text-5xl uppercase">{props.serie.name}</h1>
-                </div>
-                <div className="pt-2">
-                  <h3 className="text.md sm:text-lg text-gray-200">{genres}</h3>
-                </div>                
-                <div className="text-md sm:text-lg text-gray-200">{`${props.serie.first_air_date.split("-")[0]} - ${props.serie.last_air_date.split("-")[0]} | ${props.serie.number_of_seasons} temporadas`}</div>
-                <div className="hidden md:block pt-10">
-                  <div className="w-[300px] md:w-[600px] lg:w-[700px] 2xl:w-[1200px] 3xl:w-[1500px] max-h-20 lg:max-h-56 2xl:max-h-64 overflow-y-auto text-justify ">
-                    <h2 className="text-lg sm:text-lg lg:text-2xl">{props.serie.overview}</h2>
+                fill
+                style={{objectFit: 'cover', objectPosition: 'center top'}} 
+                alt={props.serie.name}/>
+            <div className='gradientOverIMG'></div>
+            <div className="bgBackdrop">
+              <span className="sectionGral">
+                  <div className="titTipo">Serie</div>
+                
+                  <div className="">
+                    <h1 className="titID">{props.serie.name}</h1>
+                    <h2 className='sectionGral'>Titulo Original: {props.serie.original_name}</h2>
                   </div>
-                  <div className="pt-7"><span className="text-lg text-gray-400">Protagonistas: </span><span className="text-lg">{cast}</span></div>
+
+                  <div className="pt-2">
+                    <h3 className="textoID">{genres}</h3>
+                    <div className="textoID">{`${props.serie.first_air_date.split("-")[0]} - ${props.serie.last_air_date.split("-")[0]} | ${props.serie.number_of_seasons} temporadas`}</div>
+                  </div>
+
+                
+                
+                <div className="md:flex sm:flex-line">
+                  <div className=''>
+                      <div className="thumbID">
+                        <Image className="thumbIMGBorder" 
+                            src={`${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD}${props.serie.poster_path}`} 
+                            fill
+                            blurDataURL={`/_next/image?url=${process.env.THEMOVIEDB_BASE_URL}${process.env.THEMOVIEDB_POSTER_SIZE_CARD_MIN}${props.serie.poster_path}&w=16&q=1`} 
+                            style={{objectFit: 'cover',}} 
+                            sizes={imageSizes} alt={props.serie.name}/>    
+                      </div>
+                  </div>
+                  <div className=''> 
+                    <div className="descripcionID">
+                        <span>{props.serie.overview}</span>
+                        <div className="pt-4">
+                          <span className="textoResaltado">Protagonistas: </span>
+                          <span>{cast}</span>
+                        </div>
+                        <div>
+                          <span><span className="textoResaltado">Puntaje: </span> {Math.round(props.serie.vote_average * 10) / 10}</span>
+                        </div>
+                        <div>
+                            {
+                              props.serie['watch/providers']?.watchProviderCountry &&(
+                                <WatchProviderContainer watchProvider={props.serie['watch/providers'].watchProviderCountry}/>
+                              )
+                            }  
+                        </div> 
+                  </div>
+                  </div>
+
                 </div>
-              </section>
+              </span>
             </div>
           </div>
-          <div className="visible md:hidden bg-gray-600 text-white">
-            <div className="pt-10 text-left">
-              <h2 className="text-lg sm:text-lg lg:text-2xl text-justify">{props.serie.overview}</h2>
-            </div>
-            <div className="pt-7 text-left"><span className="text-lg text-gray-400">Protagonistas: </span><span className="text-lg">{cast}</span></div>
-          </div>   
-          <div className='bg-gray-500'>
-            <h2>Titulo Original: {props.serie.original_name}</h2>
-            <h2>Puntaje: {Math.round(props.serie.vote_average * 10) / 10}</h2>
-          </div>
-          {
-            props.serie['watch/providers']?.watchProviderCountry &&(
-              <WatchProviderContainer watchProvider={props.serie['watch/providers'].watchProviderCountry}/>
-            )
-          }   
+
       </main>     
     </>
   )
