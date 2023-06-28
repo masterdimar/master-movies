bf57afe11e1d3dc19b70b9058cc072699c609e57
Pablo Di Martino <masterdimar@gmail.com>
2023-06-28 11:25:34 -0300

    cast

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,6 +1,8 @@
 import MovieContainer from "@/common/components/movieContainer"
 import SerieContainer from "@/common/components/serieContainer"
 import { TMDBDiscover } from "@/common/types/tmdbDiscover"
+import { TMDBMovie } from "@/common/types/tmdbMovie"
+import { TMDBSerie } from "@/common/types/tmdbSerie"
 import { GetServerSideProps, GetServerSidePropsContext } from "next"
 
 type Props ={
@@ -13,8 +15,8 @@ export default function Search(props: Props) {
     console.log(props.discoverMovies)
     return (
         <div>
-            <MovieContainer language={props.language} movies={props.discoverMovies.results}/>
-            <SerieContainer language={props.language} series={props.discoverSeries.results}/>    
+            <MovieContainer language={props.language} movies={props.discoverMovies.results as TMDBMovie[]}/>
+            <SerieContainer language={props.language} series={props.discoverSeries.results as TMDBSerie[]}/>    
         </div>
     )
 }
@@ -23,18 +25,16 @@ export const getServerSideProps: GetServerSideProps = async (context: GetServerS
 {  
     const language: string = context.query?.language?.toString() || "en-US"
     const country: string = language.split("-")[1]
-    const searchTerm: string = context.query?.searchTerm?.toString();
+    const searchTerm: string = context.query?.searchTerm?.toString() || "";
 
     const movieProviders = await fetch(`${process.env.THEMOVIEDB_API_URL}/watch/providers/movie?api_key=${process.env.THEMOVIEDB_API_KEY}&language=en-US&watch_region=${country}`).then((x) => x.json());
-    const tvProviders = await fetch(`${process.env.THEMOVIEDB_API_URL}/watch/providers/tv?api_key=${process.env.THEMOVIEDB_API_KEY}&watch_region=${country}}`).then((x) => x.json());
+    const tvProviders = await fetch(`${process.env.THEMOVIEDB_API_URL}/watch/providers/tv?api_key=${process.env.THEMOVIEDB_API_KEY}&watch_region=${country}`).then((x) => x.json());
 
     const movieProvidersString : string = movieProviders.results.map((provider: any) => provider.provider_id).join("|");
     const tvProvidersString : string = tvProviders.results.map((provider: any) => provider.provider_id).join("|");
 
-    console.log(`${process.env.THEMOVIEDB_API_URL}/discover/movie?api_key=${process.env.THEMOVIEDB_API_KEY}&with_text_query=rocky&watch_region=${country}&with_watch_providers${movieProvidersString}`);
-
-    const discoverMovies: TMDBDiscover = await fetch(`${process.env.THEMOVIEDB_API_URL}/discover/movie?api_key=${process.env.THEMOVIEDB_API_KEY}&with_text_query=${searchTerm}&watch_region=${country}&with_watch_providers=${movieProvidersString}`).then((x) => x.json());
-    const discoverSeries: TMDBDiscover = await fetch(`${process.env.THEMOVIEDB_API_URL}/discover/tv?api_key=${process.env.THEMOVIEDB_API_KEY}&with_text_query=${searchTerm}&watch_region=${country}&with_watch_providers=${tvProvidersString}`).then((x) => x.json());
+    const discoverMovies: TMDBDiscover = await fetch(`${process.env.THEMOVIEDB_API_URL}/discover/movie?api_key=${process.env.THEMOVIEDB_API_KEY}&with_text_query=${searchTerm}&watch_region=${country}&with_watch_providers=${movieProvidersString}&sort_by=popularity.desc`).then((x) => x.json());
+    const discoverSeries: TMDBDiscover = await fetch(`${process.env.THEMOVIEDB_API_URL}/discover/tv?api_key=${process.env.THEMOVIEDB_API_KEY}&with_text_query=${searchTerm}&watch_region=${country}&with_watch_providers=${tvProvidersString}&sort_by=popularity.desc`).then((x) => x.json());
     
     return {
         props: {     
